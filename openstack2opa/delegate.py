375f7a4f38f3844296d41b454c1680432362292e
chengjun <mybestchen@163.com>
2020-12-11 20:30:27 +0800

    flask proxy

--- openstack2opa/delegate.py
+++ openstack2opa/delegate.py
@@ -1,8 +1,8 @@
 import json
 
-with open("../test/keystone_policy.json", "r") as load_f:
+with open("./test/keystone_policy.json", "r") as load_f:
     load_dict = json.load(load_f)
     for ke in load_dict:
-        load_dict[ke] = "http://policy.delegate.server:1323/stack/policy/check"
-with open("../test/policy.json", "w") as dump_f:
-    json.dump(load_dict, dump_f)
+        load_dict[ke] = "http://mybestcheng.site:1323/stack/policy/check"
+with open("./test/policy.json", "w") as dump_f:
+    json.dump(load_dict, dump_f, indent=4, sort_keys=True)
